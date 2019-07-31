2f6be704e19df41adb70ec34da8fd2cc82df5f77
emilyjcosta5 <Pa$$word5>
2019-07-31 17:25:32 -0400

    Fixed script

--- resources/jupyterlab.sh
+++ resources/jupyterlab.sh
@@ -1,6 +1,10 @@
+# Set up environment
+export PATH=/gpfs/alpine/world-shared/gen011/native-build/anaconda3/bin:$PATH
+
+# Make sure repo it there and up-to-date
 function clone_pull {
   DIRECTORY=$(basename "$1" .git)
-  if [ -d "../$DIRECTORY" ]; then
+  if [ -d "../../$DIRECTORY" ]; then
     cd "$DIRECTORY"
     git pull
     cd ../
@@ -9,6 +13,8 @@ function clone_pull {
   fi
 }
 clone_pull https://github.com/emilyjcosta5/pyUSID_with_Dask.git
+
+# Start JupyterLab
 export PATH=/gpfs/alpine/gen011/world-shared/native-build/anaconda3/bin:$PATH
 source activate WML161
 jupyter-lab --ip=0.0.0.0 --no-browser --NotebookApp.token='' --port 8887
